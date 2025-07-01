951ed2398916671baa8f1167644e5c90cbf66814
AlysonStahl-NOAA <166434581+AlysonStahl-NOAA@users.noreply.github.com>
2025-07-01 15:54:16 -0700

    add test

---
+++ tests/test_c_api.c
@@ -0,0 +1,26 @@
+/* This is a test of the wgrib2 c api.
+ *
+ * Alyson Stahl
+*/
+
+#include "c_wgrib2api.h"
+
+#define GRB_FILE "data/gdaswave.t00z.wcoast.0p16.f000.grib2"
+#define GRB_INV "data/gdaswave.t00z.wcoast.0p16.f000.inv"
+
+int
+main()
+{
+    printf("Testing grb2_mk_inv()...\n");
+    {
+        int ret;
+
+        if ((ret = grb2_mk_inv(GRB_FILE, GRB_INV)))
+            return ret;
+            
+    }
+    
+    printf("ok!\n");
+    printf("SUCCESS!\n");
+    return 0;
+}
