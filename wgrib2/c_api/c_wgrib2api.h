5df14ec4a0d39c91f3519ba014ec713d1c1ccefe
AlysonStahl-NOAA <166434581+AlysonStahl-NOAA@users.noreply.github.com>
2025-07-01 18:50:01 -0700

    revert debug changes and update cmake

--- wgrib2/c_api/c_wgrib2api.h
+++ wgrib2/c_api/c_wgrib2api.h
@@ -4,21 +4,7 @@
  * this include file requires C99 because it uses __VA_ARGS__
  */
 
-/* used by C and fortran callable_wgrib2 API  10/2024 Public Domain  Wesley Ebisuzaki */
-
-#include <stddef.h>
-int wgrib2(int argc, const char **argv);
-int wgrib2a(char *arg1, ...);
-
-size_t wgrib2_get_mem_buffer_size(int n);
-int wgrib2_get_mem_buffer(unsigned char *my_buffer, size_t size, int n);
-int wgrib2_set_mem_buffer(const unsigned char *my_buffer, size_t size, int n);
-
-size_t wgrib2_get_reg_size(int reg);
-int wgrib2_get_reg_data(float *data, size_t size, int reg);
-int wgrib2_set_reg(float *data, size_t size, int reg);
-
-int wgrib2_free_file(const char *filename);		// will delete files that are used/not used
+#include "wgrib2_api.h"
 
 /* these are based on wgrib2api */
 
