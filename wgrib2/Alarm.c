cf1c30038e665b986cf05e269c41279c0d55693d
AlysonStahl-NOAA <166434581+AlysonStahl-NOAA@users.noreply.github.com>
2025-08-04 13:50:31 -0700

    Update Alarm.c

--- wgrib2/Alarm.c
+++ wgrib2/Alarm.c
@@ -1,9 +1,13 @@
-/* public domain 4/2017 Wesley Ebisuzaki   Alarm.c, stop wgrib2 after N seconds */
+/** @file
+ * @brief Functions to stop wgrib2 after a specified number of seconds. 
+ * @author Public Domain: Wesley Ebisuzaki @date 04/2017
+ */
 
 #include <stdio.h>
 #include <stdlib.h>
 #include "wgrib2.h"
 #include "fnlist.h"
+
 /*
  * HEADER:100:alarm:setup:1:terminate after X seconds
  */
@@ -11,6 +15,36 @@
 #ifndef DISABLE_ALARM
 
 #include <unistd.h>
+
+/** 
+ * Terminates wgrib2 after N seconds (real time). The alarm is activated in the 
+ * setup phase and removed in the finalize phase.
+ * 
+ * @param ARG1 Arguments and context for the wgrib2 function macro. Requires a single argument which is the number of seconds to wait before terminating.
+ * 
+ * @return 0 on success, does not return error codes.
+ * 
+ * @note The code for -alarm is SVr4, BSD, POSIX-1-2001 and IEEE Std 1003.1-2001 compatible. 
+ * The code is also supported by the Cygwin system for Windows systems. The -alarm will need 
+ * to be disabled for non-compatible systems. (-DDISABLE_ALARM=on)
+ * 
+ * The current version of -alarm simply terminates the process. This may be the appropriate
+ * action when wgrib2 is taking too long. However, this heavy-handed action may not be an ideal 
+ * action for programs that are calling the wgrib2 subroutine. For these situations, the alarm should
+ * be set up by the main program so that a more appropriate action can be taken.
+ * 
+ * The -alarm option is a replacement for -limit which limits the number of (sub)messages which are 
+ * processed. Jobs on web servers may hang because of problems with the network connections which will 
+ * never trigger the -limit option. 
+ * 
+ * ## Example Usage:
+ *  -alarm N
+ * 
+ * N is an integer from 0..65536 (ISO C Standard). N = 0 will remove any pending alarm.
+ * 
+ * For Posix systems, the system generates a SIGALRM signal to the process after N seconds. The default 
+ * action is to terminate the process (wgrib2). 
+*/
 int f_alarm(ARG1) {
     int i;
     if (mode == -1) {
