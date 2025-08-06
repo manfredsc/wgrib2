8deb99b25de8c82af0bb4cd5c10170553253503a
AlysonStahl-NOAA <166434581+AlysonStahl-NOAA@users.noreply.github.com>
2025-08-06 16:53:29 -0700

    Update Dump.c

--- wgrib2/Dump.c
+++ wgrib2/Dump.c
@@ -1,3 +1,13 @@
+/** @file
+ * @brief Specifies the grib message number to process.
+ * @author Wesley Ebisuzaki @date 2004
+ * 
+ * ### Program History Log
+ * Date | Programmer | Comments
+ * -----|------------|---------
+ * 2004 | W. Ebisuzaki | Initial
+ * 3/2020 | G. Trojan | Added optional dump offset
+ */
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
@@ -9,24 +19,65 @@
 #include "wgrib2.h"
 #include "fnlist.h"
 
-
-/* 
- *  Dump.c
- *     2004        Public Domain Wesley Ebisuzaki
- *     3/2020      George Trojan: add optional dump offset
- */
-
-
-
 /*
  * HEADER:200:d:setup:1:dump message X = n, n.m, n:offset, n.m:offset, only 1 -d allowed
  */
 
-extern int dump_msg, dump_submsg;
+ /** Pointer to the grib message number to process. */
+extern int dump_msg;
+
+/** Pointer to the sub-message number to process. */
+extern int dump_submsg;
+
+/** Offset in bytes to skip before processing the dump. */
 extern size_t dump_offset;
 
+/** Input type. */
 extern enum input_type input;
 
+/** 
+ * Specifies the grib message number (N) to process. 
+ * 
+ * The -d N option comes from wgrib where "d" stood fom "dump". 
+ * 
+ * If Nth message has submessage, then the first submessage is chosen (N.1). For messages 
+ * with submessages, use -d N.M to select the Mth submessage of the Nth message. 
+ * If M is missing, only the first submessage is selected. 
+ * 
+ * You can also add an optional offset. For example, -d N:OFFSET will skip OFFSET bytes 
+ * (must be positive), find the next grib message, give it a label N, and dump it. The 
+ * other form, -d N.M:OFFSET will skip OFFSET bytes (must be positive), find the next grib 
+ * message and dump the Mth submessage of that grib message. Using the offset option can be 
+ * faster for large files, and can be used to skip sections of a grib file that stop wgrib2 
+ * processing. The -d option works for files and pipes.
+ * 
+ * If there are more than one -d on a command line, only the last one is used. 
+ * 
+ * The form, -d all, is not valid in wgrib2. This is the default action. 
+ * 
+ * The -d should be used with caution. The order of messages records within grib files can 
+ * change without notice (ex. today's forecast may have a different order from tomorrow's forecast). 
+ * I use -d interactively but this option should not be used in scripts unless you are 100% 
+ * certain of the order of the records. 
+ * 
+ * @param ARG1 Arguments and context for the wgrib2 function macro. Requires one argument in the
+ * form of N, N.M, N:OFFSET, or N.M:OFFSET.
+ * 
+ * @return 0 on success, error code otherwise.
+ * 
+ * ## Usage:
+ * -d N
+ * -d N.M
+ * -d N:OFFSET
+ * -d N.M:OFFSET
+ * 
+ * N is an integer larger than 0, M is an positive integer, OFFSET is a positive integer
+ * 
+ * ## Example:
+ * ???
+ * 
+ * @author Wesley Ebisuzaki @date 2004
+*/
 int f_d(ARG1) {
     const char *s;
 
