122a589d2680cee01e44fc7cf949eba1099db932
AlysonStahl-NOAA <166434581+AlysonStahl-NOAA@users.noreply.github.com>
2025-08-06 17:25:28 -0700

    Update End.c

--- wgrib2/End.c
+++ wgrib2/End.c
@@ -1,3 +1,7 @@
+/** @file
+ * @brief Stops the processing of the grib file.
+ * @author Public Domain: Wesley Ebisuzaki @date 02/2008
+ */
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
@@ -10,7 +14,26 @@
 /*
  * HEADER:100:end:misc:0:stop after first (sub)message (save time)
  */
-extern unsigned int last_message;                /* last message to process */
+
+/* Last message to process. */
+extern unsigned int last_message;
+
+/**
+ * Stops the processing of the grib file after one line of the inventory has been written. 
+ * This option is designed to improve speed when used with the -match option.
+ * 
+ * @param ARG0 Arguments and context for the wgrib2 function macro.
+ * 
+ * @return 0 on success.
+ * 
+ * ## Usage:
+ * -end
+ * 
+ * ## Example:
+ * ???
+ * 
+ * @author Wesley Ebisuzaki @date 02/2008
+ */
 int f_end(ARG0) {
     // if last message is already set,  don't change error code
     if (mode >= 0 && last_message == 0) last_message = 1;
