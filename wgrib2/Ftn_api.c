90f0d36f37eba9a9102ab7823fb74eedfd2a8b07
AlysonStahl-NOAA <166434581+AlysonStahl-NOAA@users.noreply.github.com>
2025-08-20 12:05:58 -0700

    format fixes and doxygen

--- wgrib2/Ftn_api.c
+++ wgrib2/Ftn_api.c
@@ -1,3 +1,8 @@
+/** @file
+ * @brief Routine called by grb2_inq in Fortran API.
+ * @author Public Domain: Wesley Ebisuzaki @date 05/2015
+ */
+
 #include <stdio.h>
 #include <stdlib.h> 
 #include <string.h>
@@ -6,17 +11,46 @@
 #include "fnlist.h"
 #include "grid_id.h"
 
-/* Ftn_api.c   5/2015 Public Domain Wesley Ebisuzaki
- *
- * this routine is called by grb2_inq - returns often needed information
- *
- */
-
+/** Current output order type. */
 extern enum output_order_type output_order;
-extern unsigned int npnts, nx_, ny_;
-extern int submsg, msg_no, inv_no;
+
+/** Number of points in the grid. */
+extern unsigned int npnts; 
+
+/** Number of grid points in the x direction. */
+extern unsigned int nx_;
+
+/** Number of grid points in the y direction. */
+extern unsigned int ny_;
+
+/** Submessage number. */
+extern int submsg;
+
+/** Message number. */
+extern int msg_no;
+
+/** Inventory number. */
+extern int inv_no;
+
 /*
  * HEADER:100:ftn_api_fn0:inv:0:n npnts nx ny msg_no submsg i11,5(1x,i11)
+ */
+
+/**
+ * Returns often needed information about a grib message.
+ * 
+ * This option is used by the grb2_inq function in the ftn_api for reading grib. Since
+ * the ftn_api and wgrib2 are part of the same package, there is no need for compatibility 
+ * between versions of wgrib2. Use of this option is not recommended. The current output is 
+ * by this C statement:
+ * 
+ * sprintf(inv_out, "%8d %8u %8u %8u %8d %8d",inv_no,npnts,nx_,ny_,msg_no, submsg)
+ * 
+ * @param ARG0 ???
+ * 
+ * @return 0 on success
+ * 
+ * @author Wesley Ebisuzaki @date 05/2015
  */
 int f_ftn_api_fn0(ARG0) {
     /* ndata data may be undefined when data is not decoded */
