5cd7c8267a412554bfd38b0854289fede5a0bc33
Manfred Schwarb <11859839+manfredsc@users.noreply.github.com>
2025-09-05 15:37:28 +0200

    Factor out multiple definitions of pi constants into an own header wmath.h. At the same time, add DEG_TO_RAD and RAD_TO_DEG for use in further cleanups. Constants are calulated with bc as follows: bc -l <<<'scale=30;n=20;v=4*a(1)+1/10^n/2;  scale=n;"#define M_PI           ";print v/1;print "  /* pi */\n"'      | sed 's/ [.]/ 0./' bc -l <<<'scale=30;n=20;v=4*a(1)/2+1/10^n/2;scale=n;"#define M_PI_2         ";print v/1;print "  /* pi/2 */\n"'    | sed 's/ [.]/ 0./' bc -l <<<'scale=30;n=20;v=4*a(1)/4+1/10^n/2;scale=n;"#define M_PI_4         ";print v/1;print "  /* pi/4 */\n"'    | sed 's/ [.]/ 0./' bc -l <<<'scale=30;n=20;v=sqrt(2)+1/10^n/2; scale=n;"#define M_SQRT2        ";print v/1;print "  /* sqrt(2) */\n"' | sed 's/ [.]/ 0./' This way no licence or copyright issues have to be considered. Luckily, the generated pi values are identical to values obtained from recent glibc math.h entries. These constants are now unconditionally defined, as multiple redundant definitions do not really hurt. Radians constants are calculated as bc -l <<<'scale=30;n=21;v=4*a(1)/180+1/10^n/2;  scale=n;"#define DEG_TO_RAD     ";print v/1;print " /* pi/180 */\n"'   | sed 's/ [.]/ 0./' bc -l <<<'scale=30;n=18;v=180/(4*a(1))+1/10^n/2;scale=n;"#define RAD_TO_DEG     ";print v/1;print "   /* 180/pi */\n"' | sed 's/ [.]/ 0./' Values are calculated with 20 significant digits to accommodate x87 80bit excess precision. Both constants are also defined in the headers of the old API of the optional Proj4 library. The new proj API (to be implemented in wgrib2) does not define radians constants and does not include math.h. Therefore add a include statement of math.h to Proj4.c.

---
+++ wgrib2/wmath.h
@@ -0,0 +1,22 @@
+/*
+ * public domain 9/2025 M. Schwarb
+ */
+
+#ifndef _WMATH_H_
+#define _WMATH_H_
+
+/*
+ * math constants usually provided by math.h, define them in any case as not ANSI C.
+ * Values are calculated with bc command, correct up to 20 significant digits to
+ * accommodate 80bit excess precision and are identical with recent glibc values.
+ */
+#define M_PI           3.14159265358979323846  /* pi */
+#define M_PI_2         1.57079632679489661923  /* pi/2 */
+#define M_PI_4         0.78539816339744830962  /* pi/4 */
+#define M_SQRT2        1.41421356237309504880  /* sqrt(2) */
+
+/* constants for conversion from and to degrees and radians */
+#define DEG_TO_RAD     0.017453292519943295769 /* pi/180 */
+#define RAD_TO_DEG     57.295779513082320877   /* 180/pi */
+
+#endif /* _WMATH_H_ */
