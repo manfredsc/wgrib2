084a4e894bfa7dcf8f1b95bbf26da19cdfcbc51b
AlysonStahl-NOAA <166434581+AlysonStahl-NOAA@users.noreply.github.com>
2025-09-16 09:22:49 -0700

    Update test_fortran_api.F90

--- tests/test_fortran_api.F90
+++ tests/test_fortran_api.F90
@@ -8,11 +8,36 @@ program test_fortran_api
    parameter(GRB2_INV = 'junk_ftn_api.inv')
    character(*) :: EXP_GRB2_INV
    parameter(EXP_GRB2_INV = 'data/ref_gdaswave.t00z.wcoast.0p16.f000.grib2.inv')
+   logical :: use_ncep_table = .false.
 
+   logical :: ret
    integer :: iret
-   
-   iret = grb2_mk_inv(GRB2_FILE, GRB2_INV, .false.)
-   if (iret .ne. 0) stop 2
+
+   print *, "Testing Fortran API..."
+
+   print *, "Testing grb2_DEFINED_VAL() and grb2_UNDEFINED_VAL()..."
+
+   ret = grb2_DEFINED_VAL(9.998e20)
+   if (.not. ret) stop 2
+
+   ret = grb2_DEFINED_VAL(9.99911e20)
+   if (.not. ret) stop 3
+
+   ret = grb2_DEFINED_VAL(grb2_UNDEFINED)
+   if (ret) stop 4
+
+   ret = grb2_UNDEFINED_VAL(9.998e20)
+   if (ret) stop 5
+
+   ret = grb2_UNDEFINED_VAL(9.99911e20)
+   if (ret) stop 6
+
+   ret = grb2_UNDEFINED_VAL(grb2_UNDEFINED)
+   if (.not. ret) stop 7
+
+   print *, "Testing grb2_mk_inv()..."
+   iret = grb2_mk_inv(GRB2_FILE, GRB2_INV, use_ncep_table)
+   if (iret .ne. 0) stop 11
    
    print *, 'SUCCESS!'
 end program test_fortran_api
