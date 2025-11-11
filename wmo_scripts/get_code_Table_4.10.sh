588605adfe36729642a7a106cfe839d073ac841a
Manfred Schwarb <11859839+manfredsc@users.noreply.github.com>
2025-11-11 12:49:48 +0100

    Add NCEP specific entries to code tables 4.7 and 4.10 and their fetch scripts. Delete corresponding *need_to_edit.dat code table files.

--- wmo_scripts/get_code_Table_4.10.sh
+++ wmo_scripts/get_code_Table_4.10.sh
@@ -13,6 +13,7 @@ outfile="CodeTable_4.10.dat"
 if [ -f "$outfile" ]; then mv "$outfile" "$outfile.old"; fi
 
 #---GRIB2 Code Table 4.10: Type of statistical processing
+#---NCEP additions from https://www.nco.ncep.noaa.gov/pmb/docs/grib2/grib2_doc/grib2_table4-10.shtml
 wget -nv "$urlbase/raw/master/GRIB2_CodeFlag_4_10_CodeTable_en.csv" -O- | sed '{
     s/, /# /g
     s/,/;/g
@@ -21,7 +22,7 @@ wget -nv "$urlbase/raw/master/GRIB2_CodeFlag_4_10_CodeTable_en.csv" -O- | sed '{
   }' | env LC_ALL=en_US iconv -c -f UTF8 -t ASCII//TRANSLIT | awk -F";" '
   {
     num=$3; name=$5
-    if (num !="" && num !~ "-" && num !~ "Code") {
+    if (num != "" && num !~ "-" && num !~ "Code") {
       switch (num) {
         case   0: wgrib2name="ave"; break
         case   1: wgrib2name="acc"; break
@@ -42,6 +43,26 @@ wget -nv "$urlbase/raw/master/GRIB2_CodeFlag_4_10_CodeTable_en.csv" -O- | sed '{
         case 102: wgrib2name="index processing"; break
         case 255: wgrib2name="missing"; break
         default: { print "ERROR: missing switch statement for",num > "/dev/stderr"; exit 1 }
+      }
+      if (num==255) {  # prepend custom NCEP entries before case 255
+        print "case   192: if (center == NCEP) string=\"climatological mean\"; break;"
+        print "case   193: if (center == NCEP) string=\"average of forecasts\"; break;"
+        print "case   194: if (center == NCEP) string=\"average of uninitialized analyses\"; break;"
+        print "case   195: if (center == NCEP) string=\"average of forecast accumulations\"; break;"
+        print "case   196: if (center == NCEP) string=\"average of cuccessive forecast accumulations\"; break;"
+        print "case   197: if (center == NCEP) string=\"average of forecast averages\"; break;"
+        print "case   198: if (center == NCEP) string=\"average of successive forecast averages\"; break;"
+        print "case   199: if (center == NCEP) string=\"climatological average of analyses\"; break;"
+        print "case   200: if (center == NCEP) string=\"climatological average of forecasts\"; break;"
+        print "case   201: if (center == NCEP) string=\"climatological RMS\"; break;"
+        print "case   202: if (center == NCEP) string=\"climatological StdDev of forecasts\"; break;"
+        print "case   203: if (center == NCEP) string=\"climatological StdDev of analyses\"; break;"
+        print "case   204: if (center == NCEP) string=\"average of forecast accumulations in 6h intervals\"; break;"
+        print "case   205: if (center == NCEP) string=\"average of forecast averages in 6h intervals\"; break;"
+        print "case   206: if (center == NCEP) string=\"average of forecast accumulations in 12h intervals\"; break;"
+        print "case   207: if (center == NCEP) string=\"average of forecast averages in 12h intervals\"; break;"
+        print "case   208: if (center == NCEP) string=\"variance\"; break;"
+        print "case   209: if (center == NCEP) string=\"coefficient\"; break;"
       }
       printf "case %5d: string=\"%s\"; break;  // %s\n",num,wgrib2name,name
     }
--- wmo_scripts/get_code_Table_4.7.sh
+++ wmo_scripts/get_code_Table_4.7.sh
@@ -13,6 +13,7 @@ outfile="CodeTable_4.7.dat"
 if [ -f "$outfile" ]; then mv "$outfile" "$outfile.old"; fi
 
 #---GRIB2 Code Table 4.7: Derived forecast
+#---NCEP additions from https://www.nco.ncep.noaa.gov/pmb/docs/grib2/grib2_doc/grib2_table4-7.shtml
 wget -nv "$urlbase/raw/master/GRIB2_CodeFlag_4_7_CodeTable_en.csv" -O- | sed '{
     s/, /# /g
     s/,/;/g
@@ -21,7 +22,7 @@ wget -nv "$urlbase/raw/master/GRIB2_CodeFlag_4_7_CodeTable_en.csv" -O- | sed '{
   }' | env LC_ALL=en_US iconv -c -f UTF8 -t ASCII//TRANSLIT | awk -F";" '
   {
     num=$3; name=$5
-    if (num !="" && num !~ "-" && num !~ "Code") {
+    if (num != "" && num !~ "-" && num !~ "Code") {
       if (num==255) {  # prepend custom NCEP entries before case 255
         print "case   192: if (center == NCEP) string=\"Unweighted Mode of All Members\"; break;"
         print "case   193: if (center == NCEP) string=\"Percentile value (10%) of All Members\"; break;"
@@ -29,6 +30,13 @@ wget -nv "$urlbase/raw/master/GRIB2_CodeFlag_4_7_CodeTable_en.csv" -O- | sed '{
         print "case   195: if (center == NCEP) string=\"Percentile value (90%) of All Members\"; break;"
         print "case   196: if (center == NCEP) string=\"Statistically decided weights for each ensemble member\"; break;"
         print "case   197: if (center == NCEP) string=\"Climate Percentile (percentile values from climate distribution)\"; break;"
+        print "case   198: if (center == NCEP) string=\"Deviation of Ensemble Mean from Daily Climatology\"; break;"
+        print "case   199: if (center == NCEP) string=\"Extreme Forecast Index\"; break;"
+        print "case   200: if (center == NCEP) string=\"Equally Weighted Mean\"; break;"
+        print "case   201: if (center == NCEP) string=\"Percentile value (5%) of All Members\"; break;"
+        print "case   202: if (center == NCEP) string=\"Percentile value (25%) of All Members\"; break;"
+        print "case   203: if (center == NCEP) string=\"Percentile value (75%) of All Members\"; break;"
+        print "case   204: if (center == NCEP) string=\"Percentile value (95%) of All Members\"; break;"
       }
       printf "case %5d: string=\"%s\"; break;\n",num,name
     }
