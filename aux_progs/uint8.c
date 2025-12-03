291ee2649308e7c66b0cfb51ccfb864d51ed8f10
AlysonStahl-NOAA <166434581+AlysonStahl-NOAA@users.noreply.github.com>
2025-12-02 17:57:35 -0800

    debugging uint8

--- aux_progs/uint8.c
+++ aux_progs/uint8.c
@@ -28,7 +28,12 @@
  */
 unsigned long int uint8(unsigned char *p) {
 
+    // Debug output
+    printf("DEBUG uint8: ULONG_MAX = %lu\n", ULONG_MAX);
+    printf("DEBUG uint8: Condition (ULONG_MAX == 4294967295UL) = %d\n", (ULONG_MAX == 4294967295UL));
+
 #if (ULONG_MAX == 4294967295UL) 
+    printf("DEBUG uint8: Taking 32-bit path\n");
     if (p[0] || p[1] || p[2] || p[3]) {
         fprintf(stderr,"unsigned value (8 byte integer) too large for machine\n");
         fprintf(stderr,"fatal error .. run on 64-bit machine\n");
@@ -37,9 +42,12 @@ unsigned long int uint8(unsigned char *p) {
     return  ((unsigned long int)p[4] << 24) + ((unsigned long int)p[5] << 16) + 
                 ((unsigned long int)p[6] << 8) + (unsigned long int)p[7];
 #else
-    return  ((unsigned long int)p[0] << 56) + ((unsigned long int)p[1] << 48) + 
+    printf("DEBUG uint8: Taking 64-bit path\n");
+    unsigned long result = ((unsigned long int)p[0] << 56) + ((unsigned long int)p[1] << 48) + 
                 ((unsigned long int)p[2] << 40) + ((unsigned long int)p[3] << 32) + 
                 ((unsigned long int)p[4] << 24) + ((unsigned long int)p[5] << 16) +
                 ((unsigned long int)p[6] << 8) + (unsigned long int)p[7];
+    printf("DEBUG uint8: Calculated result = 0x%lX\n", result);
+    return result;
 #endif
 }
