7bdf79be1a9dee49590271e127e60f54e374314b
AlysonStahl-NOAA <166434581+AlysonStahl-NOAA@users.noreply.github.com>
2025-12-15 15:33:13 -0800

    Update test_c_api.c

--- tests/test_c_api.c
+++ tests/test_c_api.c
@@ -14,8 +14,7 @@
 #define GRB_INV "junk_ftn_api.inv"
 #define EXP_GRB_IN "data/ref_gdaswave.t00z.wcoast.0p16.f000.grib2.inv"
 
-//extern jmp_buf fatal_err;
-static jmp_buf exit_jmp;
+extern jmp_buf fatal_err;
 
 int
 main()
@@ -47,13 +46,14 @@ main()
         fflush(stdout);
 
         wgrib2_init_cmds();
-        if (setjmp(exit_jmp) == 0) {
+        if (setjmp(fatal_err) == 0) {
             char longopt[CMD_LEN + 1];
             memset(longopt, 'A', CMD_LEN);
             longopt[CMD_LEN] = '\0';
-            wgrib2_add_cmd(longopt); /* should trigger fatal_error → exit() */
-            printf("ERROR: wgrib2_add_cmd() accepted an overly long string\n");
-            return 10;
+            wgrib2_add_cmd(longopt);
+            printf("ERROR: wgrib2_add_cmd() did not error on long option\n");
+            fflush(stdout);
+            return 11;
         } 
         
         /** 
