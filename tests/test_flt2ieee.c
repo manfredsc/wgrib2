83860afe48bbf98f9073e4b6379915c22ccfcc06
AlysonStahl-NOAA <166434581+AlysonStahl-NOAA@users.noreply.github.com>
2025-12-17 15:41:00 -0800

    initial test for flt2ieee

---
+++ tests/test_flt2ieee.c
@@ -0,0 +1,27 @@
+/**
+ * This tests the the functions in flt2ieee.c and flt2ieee_nan.c.
+ * 
+ * Alyson Stahl, 12/2025
+ */
+
+#include <stdio.h>
+#include "wgrib2.h"
+
+int
+main(){
+    printf("Testing flt2ieee()...\n");
+    {
+        float x;
+        unsigned char buf[4], exp_buf[4];
+        
+        /** Test with x = 0.0 */
+        x = 0.0;
+        flt2ieee(x, buf);
+        exp_buf[0] = 0x00;
+        exp_buf[1] = 0x00;
+        exp_buf[2] = 0x00;
+        exp_buf[3] = 0x00;
+        if (memcmp(buf, exp_buf, 4) != 0) return 10;
+           
+    }
+}
