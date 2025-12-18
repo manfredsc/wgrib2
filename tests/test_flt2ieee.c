fca7fb7ae04cbb6d77ba6fed1df2c09e0b36fe0b
AlysonStahl-NOAA <166434581+AlysonStahl-NOAA@users.noreply.github.com>
2025-12-17 16:19:23 -0800

    replace tests to use ieee2flt for comparison

--- tests/test_flt2ieee.c
+++ tests/test_flt2ieee.c
@@ -1,5 +1,7 @@
 /**
- * This tests the the functions in flt2ieee.c and flt2ieee_nan.c.
+ * This tests the functions converting between ieee and float.
+ * 
+ * Includes flt2ieee(), flt2ieee_nan(), ieee2flt(), and ieee2ieee_nan()
  * 
  * Alyson Stahl, 12/2025
  */
@@ -11,33 +13,15 @@
 
 int
 main(){
-    printf("Testing flt2ieee()...\n");
+    printf("Testing flt2ieee() and ieee2flt()...\n");
     {
-        float x;
-        unsigned char buf[4], exp_buf[4];
-        
+        float in_x, out_x;
+        unsigned char buf[4];
+
         printf("    Test with x = 0.0\n");
-        x = 0.0;
-        flt2ieee(x, buf);
-        exp_buf[0] = 0x00;
-        exp_buf[1] = 0x00;
-        exp_buf[2] = 0x00;
-        exp_buf[3] = 0x00;
-        if (memcmp(buf, exp_buf, 4) != 0) return 10;
-
-        printf("    Test case where exp < 0\n");
-        x = -ldexpf(1.0f, -129);
-        flt2ieee(x, buf);
-        exp_buf[0] = 0x80;
-        if (memcmp(buf, exp_buf, 4) != 0) return 11;
-
-        //printf("    Test case where exp > 255\n");
-        //x = ldexpf(1.0f, 129);
-        //flt2ieee(x, buf);
-        //exp_buf[0] = 0x7f;
-        //exp_buf[1] = 0x80;
-        //exp_buf[2] = 0x00;
-        //exp_buf[3] = 0x00;
-        //if (memcmp(buf, exp_buf, 4) != 0) return 12;
+        in_x = 0.0;
+        flt2ieee(in_x, buf);
+        out_x = ieee2flt(buf);
+        if (in_x != out_x) return 10;
     }
 }
