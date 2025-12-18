46e37df7d7c8817b38c2e1ab945d4c87521f5442
AlysonStahl-NOAA <166434581+AlysonStahl-NOAA@users.noreply.github.com>
2025-12-17 17:29:29 -0800

    Update test_flt2ieee.c

--- tests/test_flt2ieee.c
+++ tests/test_flt2ieee.c
@@ -25,13 +25,13 @@ main(){
         if (in_x != out_x) return 10;
 
         printf("    Test with x < 0.\n");
-        in_x = -1.999999941;
+        in_x = -1.9999999;
         flt2ieee(in_x, buf);
         out_x = ieee2flt(buf);
         if (in_x != out_x) return 11;
 
         printf("    Test with x > 0.\n");
-        in_x = 1.999999941;
+        in_x = 1.9999999;
         flt2ieee(in_x, buf);
         out_x = ieee2flt(buf);
         if (in_x != out_x) return 12;
@@ -42,10 +42,15 @@ main(){
         out_x = ieee2flt(buf);
         if (out_x != 0.0f) return 13;
 
-        printf("    Test where exp > 255.\n");
-        in_x = ldexpf(1.0f, 130);
+        printf("    Test with x = infinity\n");
+        in_x = INFINITY;
         flt2ieee(in_x, buf);
         out_x = ieee2flt(buf);
+        if (in_x != out_x) return 14;
+    }
+    printf("ok!\n");
+    printf("Testing flt2ieee_nan() and ieee2flt_nan()...\n");
+    {
 
     }
     printf("ok!\n");
