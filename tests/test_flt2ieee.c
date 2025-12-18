e9a6205385cff198bb8d8ca9caba2885a269e785
AlysonStahl-NOAA <166434581+AlysonStahl-NOAA@users.noreply.github.com>
2025-12-17 16:30:10 -0800

    Update test_flt2ieee.c

--- tests/test_flt2ieee.c
+++ tests/test_flt2ieee.c
@@ -23,5 +23,11 @@ main(){
         flt2ieee(in_x, buf);
         out_x = ieee2flt(buf);
         if (in_x != out_x) return 10;
+
+        printf("    Test where exp < 0.\n");
+        in_x = -ldexpf(1.0f, -128);
+        flt2ieee(in_x, buf);
+        out_x = ieee2flt(buf);
+        if (in_x != out_x) return 11;
     }
 }
