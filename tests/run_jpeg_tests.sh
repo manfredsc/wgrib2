91d946ecff287916dfd708a17975b87e20191052
AlysonStahl-NOAA <166434581+AlysonStahl-NOAA@users.noreply.github.com>
2026-02-26 16:37:40 -0800

    change top level directory to 'src'

--- tests/run_jpeg_tests.sh
+++ tests/run_jpeg_tests.sh
@@ -4,7 +4,7 @@
 #
 # Alyson Stahl 7/26/2024
 
-n=`../wgrib2/wgrib2 -config | grep -c "Supported encoding:.*jpeg2000"`
+n=`../src/wgrib2 -config | grep -c "Supported encoding:.*jpeg2000"`
 if [ "$n" -eq 0 ] ; then
   echo "*** Not running jpeg tests"
   exit 0
@@ -14,13 +14,13 @@ echo ""
 echo "*** Running wgrib2 jpeg tests"
 
 echo "*** Running stats on large grib2 file with JPEG packing"
-../wgrib2/wgrib2 data/LARGECAT220250305_12_1443copy.grib2 -stats > junk_jpeg_stats.txt
+../src/wgrib2 data/LARGECAT220250305_12_1443copy.grib2 -stats > junk_jpeg_stats.txt
 diff -w junk_jpeg_stats.txt data/ref_LARGECAT220250305_12_1443copy_stats.txt
 echo "*** SUCCESS!"
 
 echo "*** Converting from jpeg to simple packing"
-../wgrib2/wgrib2 data/gdaswave.t00z.wcoast.0p16.f000.grib2 -set_grib_type simple -grib_out junk_jpeg2simple.grb
-../wgrib2/wgrib2 junk_jpeg2simple.grb -v2 -s >  junk_jpeg2simple.txt
+../src/wgrib2 data/gdaswave.t00z.wcoast.0p16.f000.grib2 -set_grib_type simple -grib_out junk_jpeg2simple.grb
+../src/wgrib2 junk_jpeg2simple.grb -v2 -s >  junk_jpeg2simple.txt
 touch junk_jpeg2simple.txt
 diff -w junk_jpeg2simple.txt data/ref_jpeg2simple.txt
 
