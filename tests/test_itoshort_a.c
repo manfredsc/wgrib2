ec24cc2c4ce0b962b882630360056b55a6112912
AlysonStahl-NOAA <166434581+AlysonStahl-NOAA@users.noreply.github.com>
2026-03-02 13:22:34 -0800

    Update test_itoshort_a.c

--- tests/test_itoshort_a.c
+++ tests/test_itoshort_a.c
@@ -7,16 +7,27 @@
 #include <stdio.h>
 #include "wgrib2.h"
 
+#define NUM_TESTS 5
 int
 main()
 {
     printf("Testing itoshort_a().\n");
-    printf("Testing with 0. Should return '0'...\n");
     {
-        char buf[20];
-        itoshort_a(buf, 0);
-        if (strcmp(buf, "0") != 0) {
-            return 10;
+        int vals[NUM_TESTS] = {
+            0, 123, -123, 2000, -1230000
+        };
+        char exp_str[NUM_TESTS][20] = {
+            "0", "123", "n123", "2e3", "n123e6"
+        };
+
+        for (int i = 0; i < NUM_TESTS; i++) {
+            char str[20];
+            itoshort_a(str, vals[i]);
+            if (strcmp(str, exp_str[i]) != 0) {
+                printf("Test %d failed: itoshort_a(%d) returned '%s', expected '%s'.\n", 
+                        i, vals[i], str, exp_str[i]);
+                return 10;
+            }
         }
     }
     printf("SUCCESS!\n");
