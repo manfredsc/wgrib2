043220fec86f3caaf3c0595dd73ea7a813f2632d
AlysonStahl-NOAA <166434581+AlysonStahl-NOAA@users.noreply.github.com>
2026-03-02 09:53:52 -0800

    Update test_manage_inv_out.c

--- tests/test_manage_inv_out.c
+++ tests/test_manage_inv_out.c
@@ -18,7 +18,6 @@ main()
     int i;
     const char *test_string = "some string";
 
-    char *buf = NULL;
     char *buf1 = NULL;
     char *buf2 = NULL;
 
@@ -27,6 +26,7 @@ main()
 
     if (setjmp(fatal_err) == 0) {
         new_inv_out();
+        printf("new_inv_out() did not fail as expected.\n");
         return 10;
     }
 
@@ -34,6 +34,7 @@ main()
         "Should throw a fatal_error()...\n");
 
     if (setjmp(fatal_err) == 0) {
+        printf("repeat_inv_out() did not fail as expected.\n");
         repeat_inv_out();
         return 11;
     }
@@ -41,9 +42,11 @@ main()
     printf("Testing simple call of init_inv_out()...\n");
     init_inv_out();
     if (inv_out == NULL || last_inv_out == NULL) {
+        printf("init_inv_out() did not initialize the buffers.\n");
         return 12;
     }
     if (inv_out == last_inv_out) {
+        printf("init_inv_out() did not create separate buffers.\n");
         return 13;
     }
 
@@ -56,14 +59,16 @@ main()
     inv_out[i] = '\0';
 
     printf("Testing base_inv_out(). Should return the current inv_out buffer...\n");
-    buf = base_inv_out();
-    if (buf != inv_out) {
+    buf1 = base_inv_out();
+    if (buf1 != inv_out) {
+        printf("base_inv_out() did not return the current inv_out buffer.\n");
         return 14;
     }
 
     printf("Testing initial call of new_inv_out(). Should switch to a new buffer...\n");
     new_inv_out();
-    if (inv_out == buf) {
+    if (inv_out == buf1) {
+        printf("inv_out was not switched to a new buffer.\n");
         return 15;
     }
 
@@ -74,39 +79,34 @@ main()
     printf("Calling new_inv_out() again. Should switch to a new buffer...\n");
     new_inv_out();
     if (inv_out != buf1 || last_inv_out != buf2) {
+        printf("Buffers were not switched to new buffers.\n");
         return 16;
     }
 
     printf("Testing initial call of repeat_inv_out(). Should switch to the previous buffer...\n");
     repeat_inv_out();
     if (inv_out != buf2 || last_inv_out != buf1) {
+        printf("Buffers were not switched to the previous buffers.\n");
         return 17;
     }
 
     printf("Calling repeat_inv_out() again. Should switch to the previous buffer...\n");
     repeat_inv_out();
     if (inv_out != buf1 || last_inv_out != buf2) {
+        printf("Buffers were not switched back to the previous buffers.\n");
         return 18;
-    }
-
-
-    printf("Verifying that the contents of the current inv_out buffer are unchanged...\n");
-    for (i = 0; test_string[i] != '\0'; i++) {
-        if (buf1[i] != test_string[i]) {
-            return 19;
-        }
-    }
-    if (buf1[i] != '\0') {
-        return 19;
     }
 
     printf("Testing second call to init_inv_out(). Should reset both buffers to empty strings...\n");
     init_inv_out();
     if (inv_out != buf1 || last_inv_out != buf2) {
-        return 20;
+        printf("Buffers were not reset to the same locations.\n");
+        return 19;
     }
+    
     if (inv_out[0] != '\0' || last_inv_out[0] != '\0') {
-        return 21;
+        printf("Buffers were not reset to empty strings.\n");
+        return 20;
     }
 
     printf("SUCCESS!\n");
