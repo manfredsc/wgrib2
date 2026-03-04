cdf7098edbb224bd68ee8a170b340a09033ca6d4
AlysonStahl-NOAA <166434581+AlysonStahl-NOAA@users.noreply.github.com>
2026-03-04 15:57:54 -0800

    Update test_same_sec.c

--- tests/test_same_sec.c
+++ tests/test_same_sec.c
@@ -13,51 +13,87 @@
 int
 main()
 {
-    printf("Testing same_sec*() routines.\n");
-    printf("Testing sec0 routines...\n");
+    printf("Testing same_sec0()...\n");
     {
         unsigned char sec0_a[16] = {
-            0x47, 0x52, 0x49, 0x42,     /* "GRIB" */
-            0, 0,                       /* Reserved */
-            0,                          /* Discipline */
-            2,                          /* Edition Number */
-            0, 0, 0, 0, 0, 0, 0, 0      /* Total Length (not compared) */
+            'G', 'R', 'I', 'B', 
+            0, 0,                   /* reserved */
+            0, 2,                   /* discipline = 0, edition = 2 */
+            0, 0, 0, 0, 0, 0, 0, 0  /* total length (not checked) */
         };
         unsigned char sec0_b[16] = {0};
         unsigned char *sec_a[10] = {0};
         unsigned char *sec_b[10] = {0};
 
-        for (int i = 0; i < 16; i++) sec0_b[i] = sec0_a[i];
-
+        /**
+         * same_sec0() does not compare beyond byte 8 (total length). 
+         * Test will break if same_sec0() ever compares all 16 bytes.
+         */
+        for (int i = 0; i < 8; i++) sec0_b[i] = sec0_a[i];
+        for (int i = 8; i < 16; i++) sec0_b[i] = 255;
+        
         sec_a[0] = sec0_a;
         sec_b[0] = sec0_b;
 
-        /* identical sections, should return 1 */
         if (!same_sec0(sec_a, sec_b)) {
-            printf("same_sec0: identical sections should return 1\n");
-            return 10;
+            printf("same_sec0: sections should be the same\n");
+            return 1;
         }
 
-        /* different edition number, should return 0 */
-        sec0_b[7] = 1;
-        if (same_sec0(sec_a, sec_b)) {
-            printf("same_sec0: different edition number should return 0\n");
-            return 11;
+        for (int i = 0; i < 8; i++) {
+            sec0_b[i] = 255;
+            if (same_sec0(sec_a, sec_b)) {
+                printf("same_sec0: sections should be different at byte %d\n", i);
+                return 1;
+            }
+            sec0_b[i] = sec0_a[i];
         }
+    }
+    printf("Testing same_sec0_not_var()...\n");
+    {
+        unsigned char sec0_a[16] = {
+            'G', 'R', 'I', 'B', 
+            0, 0,                   /* reserved */
+            0, 2,                   /* discipline = 0, edition = 2 */
+            0, 0, 0, 0, 0, 0, 0, 0  /* total length (not checked) */
+        };
+        unsigned char sec0_b[16] = {0};
+        unsigned char *sec_a[10] = {0};
+        unsigned char *sec_b[10] = {0};
 
-        /* identical sections, but different edition number, should return 1 */
+        /**
+         * same_sec0() does not compare beyond byte 8 (total length). 
+         * Test will break if same_sec0() ever compares all 16 bytes.
+         */
+        for (int i = 0; i < 8; i++) sec0_b[i] = sec0_a[i];
+        for (int i = 8; i < 16; i++) sec0_b[i] = 255;
+        
+        sec_a[0] = sec0_a;
+        sec_b[0] = sec0_b;
+
         if (!same_sec0_not_var(1, sec_a, sec_b)) {
-            printf("same_sec0_not_var: should ignore edition number and return 1\n");
-            return 12;
+            printf("same_sec0_not_var: sections should be the same\n");
+            return 1;
         }
 
-        /* different discipline, should return 0 */
-        sec0_b[6] = 1;
-        sec0_b[7] = 2; /* reset edition number to original value */
-        if (same_sec0_not_var(1, sec_a, sec_b)) {
-            printf("same_sec0_not_var: different discipline should return 0\n");
-            return 13;
+        /* different edition, should be skipped */
+        sec0_b[7] = 1;
+        if (!same_sec0_not_var(1, sec_a, sec_b)) {
+            printf("same_sec0_not_var: different edition should return 1\n");
+            return 1;
         }
+        sec0_b[7] = sec0_a[7];
+
+        for (int i = 0; i < 8; i++) {
+            if (i == 7) continue; /* ignores GRIB variable */
+            sec0_b[i] = 255;
+            if (same_sec0_not_var(1, sec_a, sec_b)) {
+                printf("same_sec0_not_var: sections should be different at byte %d\n", i);
+                return 1;
+            }
+            sec0_b[i] = sec0_a[i];
+        }
+
     }
 
     printf("SUCCESS!\n");
