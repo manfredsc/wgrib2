1e5e5fe44cc386674a329b40f32f35c3d65cea4c
AlysonStahl-NOAA <166434581+AlysonStahl-NOAA@users.noreply.github.com>
2026-03-04 16:12:40 -0800

    Update test_same_sec.c

--- tests/test_same_sec.c
+++ tests/test_same_sec.c
@@ -93,9 +93,147 @@ main()
             }
             sec0_b[i] = sec0_a[i];
         }
-
     }
+    printf("Testing same_sec1()...\n");
+    {
+        unsigned char sec1_a[21] = {
+            0, 0, 0, 21,    /* Section length */
+            1,              /* Section number */
+            0, 7,           /* Center */
+            0, 14,          /* Subcenter */
+            2, 0,           /* Master table and local table */
+            0,              /* Sig. of ref. time*/
+            (2025 >> 8) & 0xff, (2025 & 0xff), /* Year */
+            3, 15, 12, 0, 0, /* Month, day, hour, minute, second */
+            0, 0            /* Prod status & type of processed data */
+        };
+        unsigned char sec1_b[21] = {0};
+        unsigned char *sec_a[10] = {0};
+        unsigned char *sec_b[10] = {0};
 
+        for (int i = 0; i < 21; i++) sec1_b[i] = sec1_a[i];
+
+        sec_a[1] = sec1_a;
+        sec_b[1] = sec1_b;
+
+        if (!same_sec1(sec_a, sec_b)) {
+            printf("same_sec1: sections should be the same\n");
+            return 1;
+        }
+
+        /* different section length */
+        sec1_b[3] = 20; 
+        if (same_sec1(sec_a, sec_b)) {
+            printf("same_sec1: different section length should return 0\n");
+            return 1;
+        }
+        sec1_b[3] = sec1_a[3];
+
+        for (int i = 4; i < 21; i++) {
+            sec1_b[i] = 255;
+            if (same_sec1(sec_a, sec_b)) {
+                printf("same_sec1: sections should be different at byte %d\n", i);
+                return 1;
+            }
+            sec1_b[i] = sec1_a[i];
+        }
+    }
+    printf("testing same_sec1_not_time()...\n");
+    {
+        unsigned char sec1_a[21] = {
+            0, 0, 0, 21,    /* Section length */
+            1,              /* Section number */
+            0, 7,           /* Center */
+            0, 14,          /* Subcenter */
+            2, 0,           /* Master table and local table */
+            0,              /* Sig. of ref. time*/
+            (2025 >> 8) & 0xff, (2025 & 0xff), /* Year */
+            3, 15, 12, 0, 0, /* Month, day, hour, minute, second */
+            0, 0            /* Prod status & type of processed data */
+        };
+        unsigned char sec1_b[21] = {0};
+        unsigned char *sec_a[10] = {0};
+        unsigned char *sec_b[10] = {0};
+
+        for (int i = 0; i < 21; i++) sec1_b[i] = sec1_a[i];
+
+        sec_a[1] = sec1_a;
+        sec_b[1] = sec1_b;
+
+        if (!same_sec1_not_time(1, sec_a, sec_b)) {
+            printf("same_sec1_not_time: sections should be the same\n");
+            return 1;
+        }
+
+        /* different section length */
+        sec1_b[3] = 20; 
+        if (same_sec1_not_time(1, sec_a, sec_b)) {
+            printf("same_sec1_not_time: different section length should return 0\n");
+            return 1;
+        }
+        sec1_b[3] = sec1_a[3];
+
+        for (int i = 4; i < 21; i++) {
+            if (i >= 12 && i < 19) continue; /* ignores time stamp */
+            sec1_b[i] = 255;
+            if (same_sec1_not_time(1, sec_a, sec_b)) {
+                printf("same_sec1_not_time: sections should be different at byte %d\n", i);
+                return 1;
+            }
+            sec1_b[i] = sec1_a[i];
+        }
+
+        /* different year */
+        sec1_b[12] = (2024 >> 8) & 0xff;
+        sec1_b[13] = 2024 & 0xff;
+        if (!same_sec1_not_time(1, sec_a, sec_b)) {
+            printf("same_sec1_not_time: different year should return 1\n");
+            return 1;
+        }
+        sec1_b[12] = sec1_a[12];
+        sec1_b[13] = sec1_a[13];
+
+        /* different month */
+        sec1_b[14] = 4;
+        if (!same_sec1_not_time(1, sec_a, sec_b)) {
+            printf("same_sec1_not_time: different month should return 1\n");
+            return 1;
+        }
+        sec1_b[14] = sec1_a[14];
+
+        /* different day */
+        sec1_b[15] = 20;
+        if (!same_sec1_not_time(1, sec_a, sec_b)) {
+            printf("same_sec1_not_time: different day should return 1\n");
+            return 1;
+        }
+        sec1_b[15] = sec1_a[15];
+
+        /* different hour */
+        sec1_b[16] = 18;
+        if (!same_sec1_not_time(1, sec_a, sec_b)) {
+            printf("same_sec1_not_time: different hour should return 1\n");
+            return 1;
+        }
+        sec1_b[16] = sec1_a[16];
+
+        /* different minute */
+        sec1_b[17] = 30;
+        if (!same_sec1_not_time(1, sec_a, sec_b)) {
+            printf("same_sec1_not_time: different minute should return 1\n");
+            return 1;
+        }
+        sec1_b[17] = sec1_a[17];
+
+        /* different second */
+        sec1_b[18] = 45;
+        if (!same_sec1_not_time(1, sec_a, sec_b)) {
+            printf("same_sec1_not_time: different second should return 1\n");
+            return 1;
+        }
+        sec1_b[18] = sec1_a[18];
+        
+    }
     printf("SUCCESS!\n");
     return 0;
 }
