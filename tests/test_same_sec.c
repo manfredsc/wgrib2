bc0e670578354ac7a39edd91c92cfc9b3e672b33
AlysonStahl-NOAA <166434581+AlysonStahl-NOAA@users.noreply.github.com>
2026-03-04 16:39:05 -0800

    Update test_same_sec.c

--- tests/test_same_sec.c
+++ tests/test_same_sec.c
@@ -232,7 +232,146 @@ main()
             return 1;
         }
         sec1_b[18] = sec1_a[18];
-        
+
+    }
+    printf("Testing same_sec1_not_var()...\n");
+    {
+        unsigned char sec1_a[21] = {
+            0, 0, 0, 21,    /* Section length */
+            1,              /* Section number */
+            0, 7,           /* Center */
+            0, 14,          /* Subcenter */
+            2, 0,           /* Master table and local table */
+            0,              /* Sig. of ref. time*/
+            (2025 >> 8) & 0xff, (2025 & 0xff), /* Year */
+            3, 15, 12, 0, 0, /* Month, day, hour, minute, second */
+            0, 0            /* Prod status & type of processed data */
+        };
+        unsigned char sec1_b[21] = {0};
+        unsigned char *sec_a[10] = {0};
+        unsigned char *sec_b[10] = {0};
+
+        for (int i = 0; i < 21; i++) sec1_b[i] = sec1_a[i];
+
+        sec_a[1] = sec1_a;
+        sec_b[1] = sec1_b;
+
+        if (!same_sec1_not_var(1, sec_a, sec_b)) {
+            printf("same_sec1_not_var: sections should be the same\n");
+            return 1;
+        }
+
+        /* different section length */
+        sec1_b[3] = 20;
+        if (same_sec1_not_var(1, sec_a, sec_b)) {
+            printf("same_sec1_not_var: different section length should return 0\n");
+            return 1;
+        }
+        sec1_b[3] = sec1_a[3];
+
+        for (int i = 4; i < 21; i++) {
+            if (i == 9 || i == 10) continue; /* ignores master table and local table */
+            sec1_b[i] = 255;
+            if (same_sec1_not_var(1, sec_a, sec_b)) {
+                printf("same_sec1_not_var: sections should be different at byte %d\n", i);
+                return 1;
+            }
+            sec1_b[i] = sec1_a[i];
+        }
+
+        /* different master table */
+        sec1_b[9] = 0;
+        if (!same_sec1_not_var(1, sec_a, sec_b)) {
+            printf("same_sec1_not_var: different master table should return 1\n");
+            return 1;
+        }
+        sec1_b[9] = sec1_a[9];
+
+        /* different local table */
+        sec1_b[10] = 255;
+        if (!same_sec1_not_var(1, sec_a, sec_b)) {
+            printf("same_sec1_not_var: different local table should return 1\n");
+            return 1;
+        }
+        sec1_b[10] = sec1_a[10];
+    }
+    printf("Testing same_sec2()...\n");
+    {
+        unsigned char sec2_a[6] = {
+            0, 0, 0, 6, /* Section length */
+            2,          /* Section number */
+            0           /* Local Use */
+        };
+        unsigned char sec2_b[6] = {0};
+        unsigned char *sec_a[10] = {0};
+        unsigned char *sec_b[10] = {0};
+
+        for (int i = 0; i < 6; i++) sec2_b[i] = sec2_a[i];
+
+        sec_a[2] = sec2_a;
+        sec_b[2] = sec2_b;
+
+        if (!same_sec2(sec_a, sec_b)) {
+            printf("same_sec2: sections should be the same\n");
+            return 1;
+        }
+
+        /* different section length */
+        sec2_b[3] = 5;
+        if (same_sec2(sec_a, sec_b)) {
+            printf("same_sec2: different section length should return 0\n");
+            return 1;
+        }
+        sec2_b[3] = sec2_a[3];
+
+        for (int i = 4; i < 6; i++) {
+            sec2_b[i] = 255;
+            if (same_sec2(sec_a, sec_b)) {
+                printf("same_sec2: sections should be different at byte %d\n", i);
+                return 1;
+            }
+            sec2_b[i] = sec2_a[i];
+        }
+    }
+    printf("Testing same_sec3()...\n");
+    {
+        unsigned char sec3_a[15] = {
+            0, 0, 0, 15,    /* Section length */
+            3,              /* Section number */
+            0,              /* Source of GDT */
+            0, 0, 0, 0,     /* Number of data points */
+            0,              /* Num octets for optional list */
+            0,              /* Interpretation of optional list */
+            0, 0,           /* Grid definition template number */
+            0               /* Template */
+        };
+        unsigned char sec3_b[15] = {0};
+        unsigned char *sec_a[10] = {0};
+        unsigned char *sec_b[10] = {0};
+
+        for (int i = 0; i < 15; i++) sec3_b[i] = sec3_a[i];
+
+        if (!same_sec3(sec_a, sec_b)) {
+            printf("same_sec3: sections should be the same\n");
+            return 1;
+        }
+
+        /* different section length */
+        sec3_b[3] = 14;
+        if (same_sec3(sec_a, sec_b)) {
+            printf("same_sec3: different section length should return 0\n");
+            return 1;
+        }
+        sec3_b[3] = sec3_a[3];
+
+        for (int i = 6; i < 15; i++) {
+            sec3_b[i] = 255;
+            if (same_sec3(sec_a, sec_b)) {
+                printf("same_sec3: sections should be different at byte %d\n", i);
+                return 1;
+            }
+            sec3_b[i] = sec3_a[i];
+        }
     }
     printf("SUCCESS!\n");
     return 0;
